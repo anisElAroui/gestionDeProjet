bbc8ee146c5fea4cea46aa23d44435b4d906f403
anisElAroui <anis.el.aroui@gmail.com>
2018-04-13 10:53:56 +0100

    ajout du bundledol/ldap-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    DoL\LdapBundle\DoLLdapBundle::class => ['all' => true],
 ];
